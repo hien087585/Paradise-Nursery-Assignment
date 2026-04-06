b1408470aed19a2a526f5b34dd6e5f75b876fb38
Vũ Hiền <hien037305@gmail.com>
2026-04-06 15:24:04 +0700

    Create AboutUs.jsx

---
+++ AboutUs.jsx
@@ -0,0 +1,24 @@
+function AboutUs() {
+  return (
+    <div className="about-us">
+      <h1>About Paradise Nursery</h1>
+      <p>
+        Welcome to Paradise Nursery, your trusted destination for beautiful
+        indoor plants and gardening essentials.
+      </p>
+
+      <p>
+        We believe plants make homes healthier, greener, and more relaxing.
+        Our nursery offers a wide selection of houseplants for every type of
+        customer.
+      </p>
+
+      <p>
+        From succulents to flowering plants, Paradise Nursery is dedicated to
+        bringing nature closer to your home.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
